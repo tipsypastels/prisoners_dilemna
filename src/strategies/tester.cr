0a4bbc222dce520399bfcd9fd97bdeca08d17326
Dakota Sankey <dakotasankey@gmail.com>
2021-03-15 23:43:24 -0700

    extract temp to query

--- src/strategies/tester.cr
+++ src/strategies/tester.cr
@@ -9,8 +9,11 @@ class Tester < Strategy
     if foe.past_choices.any?(&.==(defect))
       foe.past_choices.last # become tit for tat
     else
-      defect_ratio = past_choices.count(&.==(defect)) / past_choices.size
       defect_ratio >= 0.5 ? cooperate : defect
     end
+  end
+
+  private def defect_ratio
+    past_choices.count(&.==(defect)) / past_choices.size
   end
 end
